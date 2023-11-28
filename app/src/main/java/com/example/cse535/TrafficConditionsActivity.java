6089b8d54ba0f93aae27065dd652d053328d42e4
Maxwell Berry <98619714+accfmax95@users.noreply.github.com>
2023-11-28 14:11:07 -0700

    Update TrafficConditionsActivity.java
    Page to generate the final recommendation

--- app/src/main/java/com/example/cse535/TrafficConditionsActivity.java
+++ app/src/main/java/com/example/cse535/TrafficConditionsActivity.java
@@ -6,133 +6,196 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.example.cse535.databinding.ActivityTrafficConditionsBinding;
+public class TrafficConditionsActivity extends AppCompatActivity implements DistanceMatrixAPIRequest.DistanceMatrixCallback {
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-
-public class TrafficConditionsActivity extends AppCompatActivity {
-
-    private ActivityTrafficConditionsBinding binding;
+    Button makeRequest;
+    String originOG, destinationOG, roadCondition;
+    EditText originText, desText;
+    TextView displayArea;
+    final double reactionTime = 0.7324;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityTrafficConditionsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        binding.startLocation.setText("Tempe, AZ");
-        binding.destination.setText("Scottsdale, AZ");
 
-        binding.fetchData.setOnClickListener(new View.OnClickListener() {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traffic_conditions);
+
+        originText = (EditText) findViewById(R.id.startLocation);
+        desText = (EditText) findViewById(R.id.destination);
+        displayArea = (TextView) findViewById(R.id.displayArea);
+
+        makeRequest = findViewById(R.id.fetchData);
+        originText.setText("Tempe, AZ");
+        desText.setText("Scottsdale, AZ");
+
+        makeRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new APICallTask(binding.startLocation.getText().toString(), binding.destination.getText().toString()).execute();
+
+                originOG = originText.getText().toString();
+                destinationOG = desText.getText().toString();
+
+                String origin = originOG.replace(" ", "%20").replace(",", "%2C");
+                String destination = destinationOG.replace(" ", "%20").replace(",", "%2C");
+
+                makeDistanceMatrixRequest(origin, destination);
             }
         });
     }
 
-    private class APICallTask extends AsyncTask<Void, Void, String> {
-        private String startPoint;
-        private String endPoint;
-        APICallTask(String startPoint, String endPoint) {
-            this.startPoint = startPoint;
-            this.endPoint = endPoint;
+    private void makeDistanceMatrixRequest(String origin, String destination) {
+        DistanceMatrixAPIRequest apiRequest = new DistanceMatrixAPIRequest(this);
+        apiRequest.execute(origin, destination);
+    }
+
+    @Override
+    public void onDistanceMatrixResult(DistanceMatrixResult result) {
+        if (result != null) {
+            int distance = result.getDistance();
+            int duration = result.getDuration();
+            int durationInTraffic = result.getDurationInTraffic();
+            int reactionTime = 550; // In milliseconds. This is just a testing value; actual value will be anywhere from 1-1000
+
+            Log.d("CheckDur", "Duration" + durationInTraffic);
+
+            recommendationAlgo(distance, duration, durationInTraffic, reactionTime);
+
+        } else {
+            // Handle the case where the result is null or an error occurred
+            Log.d("Error", "API Result is Null");
         }
-        @Override
-        protected String doInBackground(Void... voids) {
-            try {
-                // Define the API URL
-                String googleApiKey = "Enter-Your-API-Key"; // Replace with your actual API key
+    }
 
-                String apiUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?" +
-                        "origins=" + startPoint + "&destinations=" + endPoint +
-                        "&mode=driving&departure_time=now&key=" + googleApiKey;
+    public void recommendationAlgo(int distance, int duration, int durationInTraffic, int reactionTime) {
 
-                // Create a URL object
-                URL url = new URL(apiUrl);
+        double speedWithoutTraffic = distance / (double) duration;
+        double speedWithTraffic = distance / (double) durationInTraffic;
 
-                // Create an HttpURLConnection
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
+        // Assign threshold for reaction time (in MS)
+        int fastReactionTimeThreshold = 300;
+        int averageReactionTimeThreshold = 600;
 
-                // Read the response
-                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                StringBuilder response = new StringBuilder();
-                String line;
+        // Assigning threshold for road condition
+        double poorRoadConditionThreshold = 0.7;
 
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
-                }
+        String recommendation = "No specific recommendation";
+        if (distance >= 500000) {
 
-                reader.close();
+            // Very long trip, recommend plane or train
+            if (durationInTraffic <= 36000) {
 
-                // Return the response as a string
-                return response.toString();
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
+                // Consider train for shorter very long trips
+                if (reactionTime <= fastReactionTimeThreshold) {
 
-        @Override
-        protected void onPostExecute(String response) {
-            if (response != null) {
-                // Handle the response here
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
+                    // Very long trip with fast reaction time, recommend personal car
+                    recommendation = "You have a fast reaction time. We recommend using your personal car for this very long trip.";
+                } else if (reactionTime <= averageReactionTimeThreshold) {
 
-                    // Extract elements from the JSON response
-                    JSONArray rows = jsonObject.getJSONArray("rows");
-                    ArrayList<Integer> durations_in_traffic = new ArrayList<>();
-                    ArrayList<Integer> durations = new ArrayList<>();
-                    ArrayList<Integer> distances = new ArrayList<>();
+                    // Very long trip with average reaction time, recommend personal car or train
+                    recommendation = "You have an average reaction time. Using your personal car should be fine, but you should consider taking a train or plane for this very long trip.";
+                } else {
 
-                    for (int i = 0; i < rows.length(); i++) {
-                        JSONArray elements = rows.getJSONObject(i).getJSONArray("elements");
-                        for (int j = 0; j < elements.length(); j++) {
-                            JSONObject element = elements.getJSONObject(j);
-                            int d_in_traffic = element.getJSONObject("duration_in_traffic").getInt("value");
-                            durations_in_traffic.add(d_in_traffic);
-                            int d = element.getJSONObject("duration").getInt("value");
-                            durations.add(d);
-                            int dis = element.getJSONObject("distance").getInt("value");
-                            distances.add(dis);
-                        }
-                    }
-                    float speedInTraffic = distances.get(0)/durations_in_traffic.get(0);
-                    float speedWithoutTraffic = distances.get(0)/durations.get(0);
-                    Log.d("Response Speed in traffic:", String.valueOf(speedInTraffic) + "m/s");
-                    Log.d("Response Speed without traffic:", String.valueOf(speedWithoutTraffic) + "m/s");
-
-                    String displayText = "Speed in traffic: " + speedInTraffic + "m/s" +
-                            "\nSpeed without traffic: " + speedWithoutTraffic + "m/s";
-
-                    Float threshold = 5.0f;
-
-                    if (Math.abs(speedInTraffic - speedWithoutTraffic) > threshold) {
-                        Log.d("API Response", "Current road conditions are poor.");
-                        displayText += "\nRoad condition: Poor";
-                    } else {
-                        Log.d("API Response", "Current road conditions are normal.");
-                        displayText += "\nRoad condition: Normal";
-                    }
-                    binding.displayArea.setText(displayText);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    Log.e("API Error", "JSON parsing error");
+                    // Very long trip with poor reaction time, recommend train or plane
+                    recommendation = "Your reaction time is poor. We recommend taking a train or a plane for this very long trip.";
                 }
             } else {
-                // Handle errors or no response
-                Log.e("API Error", "API call failed");
+
+                // Consider plane for longer very long trips
+                recommendation = "For this very long trip, we recommend taking a plane for faster travel. You can use your car, but it may take well over a day.";
+            }
+        } else if (reactionTime <= fastReactionTimeThreshold) {
+
+            // Fast reaction time, consider personal car or bike
+            if (distance <= 10000) {
+
+                recommendation = "You have a fast reaction time. Since your destination is close, we recommend using a bike. " +
+                        "If you are in a hurry, your car would also be a good option";
+            } else {
+
+                recommendation = "You have a fast reaction time. Since your destination is far, we recommend using your car.";
+            }
+        } else if (reactionTime <= averageReactionTimeThreshold) {
+
+            // Average reaction time, consider road conditions
+            if (speedWithTraffic >= speedWithoutTraffic * poorRoadConditionThreshold) {
+
+                // Poor road conditions, recommend public transit options
+                if (durationInTraffic <= 1800) {
+
+                    // Short trip, consider bus, taxi, or bike
+                    if (distance <= 5000) {
+
+                        recommendation = "Road conditions are poor. We softly recommend taking a bus or taxi, though your reaction time suggests taking your car should be fine. " +
+                                "A bike may also be a good option, though it may be a long journey.";
+                    } else {
+
+                        recommendation = "Road conditions are poor. We softly recommend taking a bus or taxi, though your reaction time suggests taking your car should be fine.";
+                    }
+                } else {
+
+                    // Longer trip, consider train, Uber, or bike
+                    if (distance <= 10000) {
+
+                        recommendation = "Road conditions are poor. We softly recommend taking a train or Uber for this medium-length trip, though your reaction time suggests taking your car should be fine.";
+                    } else {
+
+                        recommendation = "Road conditions are poor. We softly recommend taking a train or Uber, though your reaction time suggests taking your car should be fine. ";
+                    }
+                }
+            } else {
+
+                // Normal road conditions, consider personal car or bike
+                if (distance <= 5000) {
+
+                    recommendation = "Road conditions are normal. We recommend using your car or bike since this is a short trip.";
+                } else {
+
+                    recommendation = "Road conditions are normal. We recommend using your car. You can also use your bike, but it may be a long journey.";
+                }
+            }
+        } else {
+
+            // Poor reaction time, recommend public transit options or bike
+            if (durationInTraffic <= 1200) {
+
+                // Short trip, recommend bus, taxi, or bike
+                if (distance <= 5000) {
+
+                    recommendation = "Your reaction time is poor. We recommend taking a bus or taxi. " +
+                            "Since your destination is somewhat close, biking may also be a good option.";
+                } else {
+
+                    recommendation = "Your reaction time is poor. We recommend taking a bus or taxi, especially since this is a long trip.";
+                }
+            } else if (durationInTraffic <= 2400) {
+
+                // Medium trip, recommend train, Uber, or bike
+                if (distance <= 10000) {
+
+                    recommendation = "Your reaction time is poor. We recommend taking a train or Uber.";
+                } else {
+
+                    recommendation = "Your reaction time is poor. We highly recommend taking a train or Uber, especially since this is a long trip.";
+                }
+            } else {
+
+                // Longer trip, recommend train, long-distance taxi, or bike
+                if (distance <= 15000) {
+
+                    recommendation = "Your reaction time is poor. We recommend taking a train or Uber since this is a longer trip.";
+                } else {
+
+                    recommendation = "Your reaction time is poor. Since this is a very long trip, we highly recommend taking a train or Uber.";
+                }
             }
         }
+
+        // Print the recommendation
+        Log.d("Recommendation", recommendation);
+        displayArea.setText(recommendation);
     }
 }
