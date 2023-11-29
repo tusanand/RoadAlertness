8012c8c02817b8fb75cac6da8ea7f4984d1932d0
Micah Ban <mtban@asu.edu>
2023-11-28 22:40:27 -0700

    added reaction time test and sleep input activity

---
+++ app/src/main/java/com/example/cse535/ReactionTimeTestActivity.java
@@ -0,0 +1,6 @@
+package com.example.cse535;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReactionTimeTestActivity extends AppCompatActivity{
+}
---
+++ app/src/main/java/com/example/cse535/SleepActivity.java
@@ -0,0 +1,6 @@
+package com.example.cse535;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SleepActivity extends AppCompatActivity {
+}
