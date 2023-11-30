0b277294839538aa8a77ebbb82cc1d335ebbfb99
Maxwell Berry <98619714+accfmax95@users.noreply.github.com>
2023-11-29 22:49:10 -0700

    Update TrafficConditionsActivity.java

--- app/src/main/java/com/example/cse535/TrafficConditionsActivity.java
+++ app/src/main/java/com/example/cse535/TrafficConditionsActivity.java
@@ -16,7 +16,6 @@ public class TrafficConditionsActivity extends AppCompatActivity implements Dist
     String originOG, destinationOG, roadCondition;
     EditText originText, desText;
     TextView displayArea;
-    final double reactionTime = 0.7324;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +38,15 @@ public class TrafficConditionsActivity extends AppCompatActivity implements Dist
                 originOG = originText.getText().toString();
                 destinationOG = desText.getText().toString();
 
-                String origin = originOG.replace(" ", "%20").replace(",", "%2C");
-                String destination = destinationOG.replace(" ", "%20").replace(",", "%2C");
+                if (originOG != null && destinationOG != null) {
 
-                makeDistanceMatrixRequest(origin, destination);
+                    String origin = originOG.replace(" ", "%20").replace(",", "%2C");
+                    String destination = destinationOG.replace(" ", "%20").replace(",", "%2C");
+                    makeDistanceMatrixRequest(origin, destination);
+                } else {
+
+                    displayArea.setText("");
+                }
             }
         });
     }
@@ -58,82 +62,88 @@ public class TrafficConditionsActivity extends AppCompatActivity implements Dist
             int distance = result.getDistance();
             int duration = result.getDuration();
             int durationInTraffic = result.getDurationInTraffic();
-            int reactionTime = 550; // In milliseconds. This is just a testing value; actual value will be anywhere from 1-1000
+            int crashSpeed = 85; // In KPH, this is just a static value for testing. Real value will range from [0-200~]
 
-            Log.d("CheckDur", "Duration" + durationInTraffic);
-
-            recommendationAlgo(distance, duration, durationInTraffic, reactionTime);
+            recommendationAlgo(distance, duration, durationInTraffic, crashSpeed);
 
         } else {
             // Handle the case where the result is null or an error occurred
             Log.d("Error", "API Result is Null");
+            displayArea.setText("There was an issue generating your recommendation. Please ensure your origin and destination are correct.");
         }
     }
 
-    public void recommendationAlgo(int distance, int duration, int durationInTraffic, int reactionTime) {
+    public void recommendationAlgo(int distance, int duration, int durationInTraffic, int crashSpeed) {
 
         double speedWithoutTraffic = distance / (double) duration;
         double speedWithTraffic = distance / (double) durationInTraffic;
 
-        // Assign threshold for reaction time (in MS)
-        int fastReactionTimeThreshold = 300;
-        int averageReactionTimeThreshold = 600;
+        if (speedWithTraffic <= speedWithoutTraffic) {
 
-        // Assigning threshold for road condition
-        double poorRoadConditionThreshold = 0.7;
+            roadCondition = "HCW";
+        } else {
 
+            roadCondition = "LCW";
+        }
+
+        // USE CRASH A KPH INSTEAD OF REACTION TIME
         String recommendation = "No specific recommendation";
         if (distance >= 500000) {
 
             // Very long trip, recommend plane or train
-            if (durationInTraffic <= 36000) {
+            if (durationInTraffic <= 72000) {
 
                 // Consider train for shorter very long trips
-                if (reactionTime <= fastReactionTimeThreshold) {
+                if (crashSpeed >= 140) {
 
-                    // Very long trip with fast reaction time, recommend personal car
-                    recommendation = "You have a fast reaction time. We recommend using your personal car for this very long trip.";
-                } else if (reactionTime <= averageReactionTimeThreshold) {
+                    // Very long trip with low crash risk, recommend personal car
+                    recommendation = "You are not at risk of crashing. We recommend using your personal car for this very long trip.";
+                } else if (crashSpeed >= 100) {
 
-                    // Very long trip with average reaction time, recommend personal car or train
-                    recommendation = "You have an average reaction time. Using your personal car should be fine, but you should consider taking a train or plane for this very long trip.";
+                    // Very long trip with mild crash risk recommend personal car or train
+                    recommendation = "You are at mild risk of crashing. Using your personal car should be fine, but you should consider taking a train or plane for this very long trip.";
+                } else if (crashSpeed >= 60) {
+
+                    recommendation = "You are at risk of crashing. You should consider taking a train or plane for this very long trip. If you do use your personal car, avoid driving on freeways and take breaks when necessary.";
                 } else {
 
-                    // Very long trip with poor reaction time, recommend train or plane
-                    recommendation = "Your reaction time is poor. We recommend taking a train or a plane for this very long trip.";
+                    // Very long trip with severe crash risk, recommend train or plane
+                    recommendation = "You are at severe risk of crashing. It is highly recommended you take a plane or train for this very long trip.";
                 }
             } else {
 
                 // Consider plane for longer very long trips
-                recommendation = "For this very long trip, we recommend taking a plane for faster travel. You can use your car, but it may take well over a day.";
+                recommendation = "For this very long trip, we recommend taking a plane for faster travel.";
             }
-        } else if (reactionTime <= fastReactionTimeThreshold) {
+        } else if (crashSpeed >= 140) {
 
-            // Fast reaction time, consider personal car or bike
-            if (distance <= 10000) {
+            // Low crash risk, consider personal car or bike
+            if (distance <= 5000) {
 
-                recommendation = "You have a fast reaction time. Since your destination is close, we recommend using a bike. " +
+                recommendation = "You are not at risk of crashing. Since your destination is close, we recommend using a bike. " +
                         "If you are in a hurry, your car would also be a good option";
             } else {
 
-                recommendation = "You have a fast reaction time. Since your destination is far, we recommend using your car.";
+                recommendation = "You are not at risk of crashing. Your car is a solid transportation option.";
             }
-        } else if (reactionTime <= averageReactionTimeThreshold) {
+
+        } else if (crashSpeed >= 100) {
 
             // Average reaction time, consider road conditions
-            if (speedWithTraffic >= speedWithoutTraffic * poorRoadConditionThreshold) {
+            if (roadCondition.equals("HCW") == true) {
 
+                Log.d("Test", speedWithTraffic + " and " + speedWithoutTraffic);
                 // Poor road conditions, recommend public transit options
                 if (durationInTraffic <= 1800) {
 
                     // Short trip, consider bus, taxi, or bike
                     if (distance <= 5000) {
 
-                        recommendation = "Road conditions are poor. We softly recommend taking a bus or taxi, though your reaction time suggests taking your car should be fine. " +
+                        recommendation = "Road conditions are poor. We softly recommend taking a bus or taxi, though taking your car should be fine. " +
                                 "A bike may also be a good option, though it may be a long journey.";
                     } else {
 
-                        recommendation = "Road conditions are poor. We softly recommend taking a bus or taxi, though your reaction time suggests taking your car should be fine.";
+                        recommendation = "Road conditions are poor. We softly recommend taking a bus or taxi, though taking your car should be fine.";
                     }
                 } else {
 
@@ -146,51 +156,114 @@ public class TrafficConditionsActivity extends AppCompatActivity implements Dist
                         recommendation = "Road conditions are poor. We softly recommend taking a train or Uber, though your reaction time suggests taking your car should be fine. ";
                     }
                 }
-            } else {
+            } else if (roadCondition.equals("LCW") == true) {
 
                 // Normal road conditions, consider personal car or bike
                 if (distance <= 5000) {
 
-                    recommendation = "Road conditions are normal. We recommend using your car or bike since this is a short trip.";
+                    recommendation = "Road conditions are normal and your crash risk is mild. We recommend using your car or bike since this is a short trip.";
                 } else {
 
-                    recommendation = "Road conditions are normal. We recommend using your car. You can also use your bike, but it may be a long journey.";
+                    recommendation = "Road conditions are normal and your crash risk is mild. We recommend using your car.";
                 }
             }
-        } else {
+
+        } else if (crashSpeed >= 60) {
 
             // Poor reaction time, recommend public transit options or bike
+            if (roadCondition.equals("LCW") == true) {
+                if (durationInTraffic <= 1200) {
+
+                    // Short trip, recommend bus, taxi, or bike
+                    if (distance <= 5000) {
+
+                        recommendation = "Road conditions are normal, but you are at risk of crashing. We recommend taking a bus or taxi. " +
+                                "Since your destination is close, it will be fine to use your personal car as long as you do not exceed " + crashSpeed + " KPH.";
+                    } else {
+
+                        recommendation = "Road conditions are normal, but you are at risk of crashing. We recommend taking a bus or taxi. " +
+                                "If you do use your personal car, it is highly recommended to avoid freeways.";
+                    }
+                } else if (durationInTraffic <= 2400) {
+
+                    // Medium trip, recommend train, Uber, or bike
+                    if (distance <= 10000) {
+
+                        recommendation = "Road conditions are normal, but you are at risk of crashing. We recommend taking a train or Uber. " +
+                                "Since your destination is fairly close, it will be fine to use your personal car as long as you do not exceed " + crashSpeed + " KPH.";
+                    } else {
+
+                        recommendation = "Road conditions are normal, but you are at risk of crashing. We recommend taking a train or Uber. " +
+                                "If you do use your personal car, it is highly recommended to avoid freeways.";
+                    }
+                } else {
+
+                    // Longer trip, recommend train, long-distance taxi, or bike
+                    if (distance <= 15000) {
+
+                        recommendation = "Road conditions are normal, but you are at risk of crashing. We recommend taking a train or Uber.";
+                    } else {
+
+                        recommendation = "Road conditions are normal, but you are at risk of crashing. We highly recommend taking a train or Uber since this is a longer trip.";
+                    }
+                }
+            } else if (roadCondition.equals("HCW") == true) {
+                if (durationInTraffic <= 1200) {
+
+                    // Short trip, recommend bus, taxi, or bike
+                    if (distance <= 5000) {
+
+                        recommendation = "Road conditions are poor and you are at risk of crashing. We recommend taking a bus or taxi. " +
+                                "Since your destination is close, biking may be a good alternative.";
+                    } else {
+
+                        recommendation = "Road conditions are poor and you are at risk of crashing. We recommend taking a bus or taxi.";
+                    }
+                } else if (durationInTraffic <= 2400) {
+
+                    // Medium trip, recommend train, Uber, or bike
+                    if (distance <= 10000) {
+
+                        recommendation = "Road conditions are poor and you are at risk of crashing. We recommend taking a train or Uber. You could consider your bike as an alternative, but it may be a long journey.";
+                    } else {
+
+                        recommendation = "Road conditions are poor and you are at risk of crashing. We recommend taking a train or Uber.";
+                    }
+                } else {
+
+                    // Longer trip, recommend train, long-distance taxi, or bike
+                    recommendation = "Road conditions are poor and you are at risk of crashing. We highly recommend taking a train or possibly plane since this is a longer trip.";
+                }
+
+            }
+
+        } else {
+
             if (durationInTraffic <= 1200) {
 
                 // Short trip, recommend bus, taxi, or bike
                 if (distance <= 5000) {
 
-                    recommendation = "Your reaction time is poor. We recommend taking a bus or taxi. " +
-                            "Since your destination is somewhat close, biking may also be a good option.";
+                    recommendation = "You are at severe risk of crashing. We highly recommend taking a bus or taxi. " +
+                            "Since your destination is close, biking may be a good alternative.";
                 } else {
 
-                    recommendation = "Your reaction time is poor. We recommend taking a bus or taxi, especially since this is a long trip.";
+                    recommendation = "You are at severe risk of crashing. We highly recommend taking a bus or taxi.";
                 }
             } else if (durationInTraffic <= 2400) {
 
                 // Medium trip, recommend train, Uber, or bike
                 if (distance <= 10000) {
 
-                    recommendation = "Your reaction time is poor. We recommend taking a train or Uber.";
+                    recommendation = "You are at severe risk of crashing. We recommend taking a train or Uber.";
                 } else {
 
-                    recommendation = "Your reaction time is poor. We highly recommend taking a train or Uber, especially since this is a long trip.";
+                    recommendation = "You are at severe risk of crashing. We highly recommend taking a train or Uber.";
                 }
             } else {
 
-                // Longer trip, recommend train, long-distance taxi, or bike
-                if (distance <= 15000) {
-
-                    recommendation = "Your reaction time is poor. We recommend taking a train or Uber since this is a longer trip.";
-                } else {
-
-                    recommendation = "Your reaction time is poor. Since this is a very long trip, we highly recommend taking a train or Uber.";
-                }
+                // Longer trip
+                recommendation = "You are at severe risk of crashing. We highly recommend taking a train or possibly plane since this is a longer trip.";
             }
         }
 
