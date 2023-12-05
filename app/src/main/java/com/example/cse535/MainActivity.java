cb9f0c3e60c7223dd1cc7e7d1395f0e023159557
amurwin <72893414+amurwin@users.noreply.github.com>
2023-12-04 19:14:57 -0700

    push

--- app/src/main/java/com/example/cse535/MainActivity.java
+++ app/src/main/java/com/example/cse535/MainActivity.java
@@ -200,11 +200,6 @@ public class MainActivity extends AppCompatActivity {
                         public void onHeartRateCalculated(String heartRate) {
                             if (heartRate != null) {
                                 heartRateValue = Integer.parseInt(heartRate);
-                                if (heartRateValue == 42) {
-
-                                } else if (heartRateValue < 60) {
-                                    heartRateValue = (int)(Math.random() * 40) + 60;
-                                }
                                 shareHeartRespiratorySleepData.setHeartRateValue(heartRateValue);
                                 binding.heartRateValue.setText("Heart rate: " + heartRateValue);
                                 binding.heartRateValue.setEnabled(true);
